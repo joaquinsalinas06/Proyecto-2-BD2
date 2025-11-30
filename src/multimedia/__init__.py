9aa6da1cfc47d7a6dcbf9fe1efaffbb00784aacd
Isaac Simeon Sarmiento <isaac.simeon@utec.edu.pe>
2025-11-30 13:08:10 -0500

    Fix

--- src/multimedia/__init__.py
+++ src/multimedia/__init__.py
@@ -1,3 +1,5 @@
 from .feature_extractors import SIFTExtractor, MFCCExtractor
+from .knn_sequential import KNNSequential
+from .knn_inverted import KNNInverted
 
-__all__ = ['SIFTExtractor', 'MFCCExtractor']
+__all__ = ['SIFTExtractor', 'MFCCExtractor', 'KNNSequential', 'KNNInverted']
